3324c6c29cdd4cdb611bb9b90a41aa37c87da741
cjh-19 <gns8812@naver.com>
2024-06-26 17:34:45 +0900

    🔀 merge: [240626] PSG_최소직사각형
    cpp

---
+++ cjh-19/algorithm/week2/PSG_최소직사각형.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// 내림차순
+bool cmp(int a, int b) {
+    return a > b;
+}
+
+int solution(vector<vector<int>> sizes) {
+    // 각 명함을 내림차순으로 정렬
+    for (int i = 0; i < sizes.size(); i++) {
+        sort(sizes[i].begin(), sizes[i].end(), cmp);
+    }
+
+    // 가로에서 가장 큰 수와 세로에서 가장 큰 수 탐색
+    int xMax = 0, yMax = 0;
+    for (int i = 0; i < sizes.size(); i++) {
+        // 가로
+        if (xMax < sizes[i][0])
+            xMax = sizes[i][0];
+        // 세로
+        if (yMax < sizes[i][1])
+            yMax = sizes[i][1];
+    }
+
+    return xMax * yMax;
+}
