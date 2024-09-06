e80b6bc3d75d58d0a7ac18bfac458064b50da7e2
cjh-19 <gns8812@naver.com>
2024-09-07 01:27:40 +0900

    🔀 merge: [240907] BOJ_토마토
    cpp

--- cjh-19/algorithm/week11/BOJ_토마토.cpp
+++ cjh-19/algorithm/week11/BOJ_토마토.cpp
@@ -39,9 +39,9 @@ void bfs() {
 
 int main() {
 	ios_base::sync_with_stdio(0); cin.tie(0);
-	int startstat = 0;
 
 	cin >> M >> N;
+	int startstat = 0;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			int stat;
